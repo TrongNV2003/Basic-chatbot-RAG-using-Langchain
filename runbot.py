50926261740039ef02f2c568aebec286b1ae0860
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-08-02 14:16:51 +0700

    Update prompt

--- runbot.py
+++ runbot.py
@@ -3,8 +3,8 @@ from chatbot import Chatbot
 bot = Chatbot()
 
 get_command = bot.runbot(
-    question="Nhân vật chính là ai?",
+    question="Tác giá của truyện này là ai?",
     
-    template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra những câu trả lời\n{context}<|im_end|>\n<|im_start|>\n<|im_start|>assistant"""
+    template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra những câu trả lời\n{context}\n<|im_start|>assistant"""
 )
 print(get_command)
