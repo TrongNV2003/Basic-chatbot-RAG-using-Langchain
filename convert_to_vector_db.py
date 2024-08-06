9f58858438bfed1ce47dffb6088e5bab7ac9ef40
Trong <trongnv.bi12-449@st.usth.edu.vn>
2024-08-06 15:25:08 +0700

    Update execute directly

--- convert_to_vector_db.py
+++ convert_to_vector_db.py
@@ -56,3 +56,15 @@ class FileVectorDB:
         db = FAISS.from_documents(chunks, embedding_model)
         db.save_local(self.vector_db_path)
         return db
+    
+if __name__ == "__main__":
+# convert a context to vectorDB
+    # context_db = ContextVectorDB()
+    # context = "Thép đã tôi thế đấy không phải là một tác phẩm văn học chỉ nhìn đời mà viết. Tác giả sống nó rồi mới viết nó. Nhân vật trung tâm Pa-ven chính là tác giả: Nhi-ca-lai A-xtơ-rốp- xki. Là một chiến sĩ cách mạng tháng Mười, ông đã sống một cách nồng cháy nhất, như nhân vật Pa-ven của ông. Cũng không phải một cuốn tiểu thuyết tự thuật thường vì hứng thú hay lợi ích cá nhân mà viết. A-xtơ-rốp-xki viết Thép đã tôi thế đấy trên giường bệnh, trong khi bại liệt và mù, bệnh tật tàn phá chín phần mười cơ thể. Chưa bao giờ có một nhà văn sáng tác trong những điều kiện gian khổ như vậy. Trong lòng người viết phải có một nhiệt độ cảm hứng nồng nàn không biết bao nhiêu mà kể. Nguồn cảm hứng ấy là sức mạnh tinh thần của người chiến sĩ cách mạng bị tàn phế, đau đớn đến cùng cực, không chịu nằm đợi chết, không thể chịu được xa rời chiến đấu, do đó phấn đấu trở thành một nhà văn và viết nên cuốn sách này. Càng yêu cuốn sách, càng kính trọng nhà văn, càng tôn quí phẩm chất của con người cách mạng."
+    # context_db.create_db_from_text(context)
+
+
+# convert all file pdf to vectorDB
+    file_db = FileVectorDB()
+    pdf_file = "1.pdf"
+    file_db.create_db_from_files(pdf_file)
